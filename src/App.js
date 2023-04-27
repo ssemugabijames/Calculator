ba590c60424ea8c12786369a40d15f2ccbbee0d1
Your Name <semugabijamws1223@gmail.com>
2023-04-27 10:47:00 +0300

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from "./components/layout";
+
+function App() {
+  return (
+    <div className="App">
+    <Layout></Layout>
+    </div>
+  );
+}
+
+export default App;
